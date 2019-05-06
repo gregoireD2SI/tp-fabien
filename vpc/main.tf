2b18dadac3afc31e65a0e9de27dbe8047d0bc355
Grégoire MONET <gregoire.monet@d2si.io>
2019-05-06 17:17:36 +0200

    created and linked public route table

--- vpc/main.tf
+++ vpc/main.tf
@@ -30,3 +30,21 @@ resource "aws_internet_gateway" "fabien_gw" {
     Name = "${var.project_name}_gw"
   }
 }
+
+resource "aws_route_table" "fabien_rt_public" {
+  vpc_id = "${aws_vpc.vpc_fabien.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.fabien_gw.id}"
+  }
+
+  tags {
+    Name = "${var.project_name}_public_rt"
+  }
+}
+
+resource "aws_route_table_association" "fabien_association_rt" {
+  subnet_id      = "${aws_subnet.fabien_sn_public.id}"
+  route_table_id = "${aws_route_table.fabien_rt_public.id}"
+}
