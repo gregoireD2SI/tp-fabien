7b864e92c957f77548fe92d0f8c818cf651e698d
Grégoire MONET <gregoire.monet@d2si.io>
2019-05-06 17:07:37 +0200

    created 2 subnets

--- vpc/main.tf
+++ vpc/main.tf
@@ -10,3 +10,15 @@ resource "aws_vpc" "vpc_fabien" {
     Name = "${var.project_name}"
   }
 }
+
+resource "aws_subnet" "fabien_sn_public" {
+  vpc_id            = "${aws_vpc.vpc_fabien.id}"
+  cidr_block        = "172.23.1.0/24"
+  availability_zone = "${var.region}a"
+}
+
+resource "aws_subnet" "fabien_sn_private" {
+  vpc_id            = "${aws_vpc.vpc_fabien.id}"
+  cidr_block        = "172.23.10.0/24"
+  availability_zone = "${var.region}b"
+}
