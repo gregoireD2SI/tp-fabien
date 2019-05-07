cd9e3ba4a5327c56e351a5360c56d623254bfdc3
Grégoire MONET <gregoire.monet@d2si.io>
2019-05-07 10:36:34 +0200

    added security group to instance (in order to get remote SSH access)

--- vpc/instances.tf
+++ vpc/instances.tf
@@ -22,3 +22,29 @@ resource "aws_eip_association" "fabien_eip_assoc" {
   instance_id   = "${aws_instance.fabien_instance_web.id}"
   allocation_id = "${aws_eip.fabien_eip.id}"
 }
+
+resource "aws_security_group" "fabien_sg_ec2" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "${aws_vpc.vpc_fabien.id}"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "allow_all_ssh"
+  }
+}
+
+
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = "${aws_security_group.fabien_sg_ec2.id}"
+  network_interface_id = "${aws_instance.fabien_instance_web.primary_network_interface_id}"
+}
+
